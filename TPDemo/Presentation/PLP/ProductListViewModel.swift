1d60e29921fb2b1ae6ce82d089dd44cd1777f29f
Jose David Sanluis <jose.sanluis@elektra.com.mx>
2023-02-26 09:59:31 -0600

    add view model

---
+++ TPDemo/Presentation/PLP/ProductListViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  ProductListViewModel.swift
+//  TPDemo
+//
+//  Created by Jose David Sanluis Castillo on 26/02/23.
+//
+
+import Foundation
+
+final class ProductListViewModel: ObservableObject {
+    @Published var products: [Product] = []
+    
+    init() {
+        self.products = getProducts()
+    }
+    
+    private func getProducts() -> [Product] {
+        
+        //TODO: add logic to get products from API
+        return [
+            .init(name: "product1", price: 1235.99, image: "IMage", rating: 4.1, description: "description"),
+            .init(name: "product1", price: 1235.99, image: "IMage", rating: 4.1, description: "description"),
+            .init(name: "product1", price: 1235.99, image: "IMage", rating: 4.1, description: "description"),
+            .init(name: "product1", price: 1235.99, image: "IMage", rating: 4.1, description: "description"),
+            .init(name: "product1", price: 1235.99, image: "IMage", rating: 4.1, description: "description"),
+            .init(name: "product1", price: 1235.99, image: "IMage", rating: 4.1, description: "description"),
+            .init(name: "product1", price: 1235.99, image: "IMage", rating: 4.1, description: "description")
+        ]
+    }
+}
